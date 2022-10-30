b9b5d386dfa83389f4f911ea28b5c9d606a071f3
brennaveir <brennaveir@hotmail.com>
2022-10-30 14:30:20 -0400

    Added functions to js file

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,20 +1,28 @@
 var topics = ["HTML", "CSS", "Git", "Javascript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let i = 0; i < topics.length; i++ ) {
+function listTopics() {
+
+    for (let i = 0; i < topics.length; i++ ) {
     console.log(topics[i]);
+    }
 }
 
-
-/* 
-if (topic === "HTML"){
+ 
+function selectTopic() {
+    if (randomTopic === "HTML") {
     console.log("Let's study HTML!");
-} else if (topic === "CSS") {
+} else if (randomTopic === "CSS") {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'Javascript') {
+} else if (randomTopic === 'Javascript') {
     console.log("Let's study Javascript");
 } else {
     console.log("Please try again");
 }
-*/
+}
+console.log("Here are the topics we learned through Prework: ");
+listTopics()
+console.log("Which topic should we study first? ");
+selectTopic()
